f81d777dc5a7e28253825a2498e62d7b699827a0
William Marques <william2391212@gmail.com>
2025-11-26 20:36:30 -0300

    update veiculo

--- src/main/java/com/br/pieng2/domain/entity/Veiculo.java
+++ src/main/java/com/br/pieng2/domain/entity/Veiculo.java
@@ -1,4 +1,19 @@
 package com.br.pieng2.domain.entity;
 
+import java.time.LocalDate;
+
 public class Veiculo {
+    private Long idVeiculo;
+    private String placa;
+    private String marca;
+    private LocalDate anoFabricacao;
+    private boolean veiculoAtivo;
+
+    public Veiculo(Long idVeiculo, String placa, String marca, LocalDate anoFabricacao, boolean veiculoAtivo) {
+        this.idVeiculo = idVeiculo;
+        this.placa = placa;
+        this.marca = marca;
+        this.anoFabricacao = anoFabricacao;
+        this.veiculoAtivo = veiculoAtivo;
+    }
 }
