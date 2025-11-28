3294dd2fd1ac826459c04f45c43bffd296dd016c
William Marques <william.2391212@gmail.com>
2025-11-28 17:11:46 -0300

    update

--- src/main/java/com/br/pieng2/domain/dao/MovimentacaoDAO.java
+++ src/main/java/com/br/pieng2/domain/dao/MovimentacaoDAO.java
@@ -1,4 +1,53 @@
 package com.br.pieng2.domain.dao;
 
-public class MovimentacaoDAO {
+import com.br.pieng2.domain.model.Movimentacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovimentacaoDAO extends BaseDAO<Movimentacao>{
+
+    private final String ARQUIVO = "movimentacao.txt";
+
+    public void salvar(Movimentacao m) {
+        List<Movimentacao> lista = listar();
+        lista.add(m);
+        gravarArquivo(ARQUIVO, lista);
+    }
+
+    public List<Movimentacao> listar() {
+        List<String> linhas = lerArquivo(ARQUIVO);
+        List<Movimentacao> lista = new ArrayList<>();
+
+        for (String l : linhas) {
+            lista.add(Movimentacao.fromString(l));
+        }
+        return lista;
+    }
+
+    public Movimentacao buscar(int id) {
+        for (Movimentacao m : listar()) {
+            if (m.getIdMovimentacao() == id)
+                return m;
+        }
+        return null;
+    }
+
+    public void atualizar(Movimentacao atualizado) {
+        List<Movimentacao> lista = listar();
+
+        for (int i = 0 ; i < lista.size(); i++) {
+            if (lista.get(i).getIdMovimentacao() == atualizado.getIdMovimentacao()) {
+                lista.set(i, atualizado);
+                break;
+            }
+        }
+        gravarArquivo(ARQUIVO, lista);
+    }
+
+    public  void remover(int id) {
+        List<Movimentacao> lista = listar();
+        lista.removeIf(m -> m.getIdMovimentacao() == id);
+        gravarArquivo(ARQUIVO, lista);
+    }
 }
--- src/main/java/com/br/pieng2/domain/service/VeiculoService.java
+++ src/main/java/com/br/pieng2/domain/service/VeiculoService.java
@@ -1,4 +1,72 @@
 package com.br.pieng2.domain.service;
 
+import com.br.pieng2.domain.dao.VeiculoDAO;
+import com.br.pieng2.domain.model.Veiculo;
+
+import java.util.List;
+
 public class VeiculoService {
+
+    private VeiculoDAO dao;
+
+    public VeiculoService() {
+        this.dao = new VeiculoDAO();
+    }
+
+    public void adicionarVeiculo(String placa, String marca, String modelo, int anoFabricacao, boolean veiculoAtivo) throws Exception {
+        if (placa == null || placa.trim().isEmpty()) {
+            throw new Exception("A placa não pode ser vazia.");
+        }
+        if (marca == null || marca.trim().isEmpty()) {
+            throw new Exception("A marca não pode ser vazia");
+            }
+        if (modelo == null || modelo.trim().isEmpty()) {
+            throw new Exception("O modelo não pode ser vazio");
+        }
+        if (anoFabricacao < 1900 || anoFabricacao >2100) {
+            throw new Exception("Ano de fabricação invalido");
+        }
+
+        for (Veiculo v : dao.listarTodos()) {
+            if (v.getPlaca().equalsIgnoreCase(placa)) {
+                throw new Exception("Já existe um veículo com essa placa.");
+            }
+        }
+
+        Veiculo veiculo = new Veiculo(placa.trim(), marca.trim(), modelo.trim(), dao.salvar(veiculo);
+    }
+
+    public List<Veiculo> listarTodos() {
+        return dao.listarTodos();
+    }
+
+    public Veiculo buscarPorPlaca(String placa) throws Exception {
+        if (placa == null || placa.isEmpty()) {
+            throw new Exception("Informe uma placa válida.");
+        }
+
+        for (Veiculo v : dao.listarTodos()) {
+            if (v.getPlaca().equalsIgnoreCase(placa)) {
+                return v;
+            }
+        }
+
+        throw new Exception("Veículo não encontrado.");
+    }
+
+    public void atualizarVeiculo(Veiculo atualizado) throws Exception {
+        if (atualizado.getPlaca() == null || atualizado.getPlaca().trim().isEmpty()) {
+            throw new Exception("A placa não pode ser vazia.");
+        }
+
+        dao.atualizar(atualizado);
+    }
+
+    public void remover(String placa) throws Exception {
+        if (placa == null || placa.isEmpty()) {
+            throw new Exception("Informe uma placa para remover.");
+        }
+
+        dao.remover(placa);
+    }
 }
